db54029e7954477dab84bbb44a0292763b84dda0
Samin-FS <65648364+samin-fs@users.noreply.github.com>
2020-12-09 10:48:21 +0330

    export symbol_icon (#62)

--- lib/deriv_chart.dart
+++ lib/deriv_chart.dart
@@ -27,4 +27,5 @@ export 'src/widgets/market_selector/market_selector.dart';
 export 'src/widgets/market_selector/market_selector_button.dart';
 export 'src/widgets/market_selector/models.dart';
 export 'src/widgets/market_selector/symbol_svg_picture.dart';
+export 'src/widgets/market_selector/symbol_icon.dart';
 export 'generated/l10n.dart';
