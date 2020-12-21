9aa4f5e5e5ac10e1132c33be355b32ed85469585
Samin-FS <65648364+samin-fs@users.noreply.github.com>
2020-12-21 07:59:04 +0330

    samin/fix_current_tick_problem_in_small_devices (#57)
    * paint tick before chnaging the y
    
    * working ...
    
    * add defaultdefault variable
    
    * remove print :D
    
    * change the name to _minPadding
    
    * remove ( :D

--- lib/src/chart.dart
+++ lib/src/chart.dart
@@ -179,6 +179,9 @@ class _ChartImplementationState extends State<_ChartImplementation>
   /// Quote scaling (drag on quote area) is controlled by this variable.
   double verticalPaddingFraction = 0.1;
 
+  /// it should be at least LabelHeight/2
+  static const double _minPadding = 12;
+
   /// Duration of quote bounds animated transition.
   final quoteBoundsAnimationDuration = const Duration(milliseconds: 300);
 
@@ -218,9 +221,10 @@ class _ChartImplementationState extends State<_ChartImplementation>
   double get _verticalPadding {
     final double padding = verticalPaddingFraction * canvasSize.height;
     const double minCrosshairPadding = 80;
-    return padding +
+    final double paddingValue = padding +
         (minCrosshairPadding - padding).clamp(0, minCrosshairPadding) *
             _crosshairZoomOutAnimation.value;
+    return paddingValue.clamp(_minPadding, canvasSize.height / 2);
   }
 
   double get _topPadding => _verticalPadding;
