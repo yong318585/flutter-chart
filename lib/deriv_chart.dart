4b3abfaa822d54e53ee02d2641d5479dd559bb32
raminvakili-fs <55975218+raminvakili-fs@users.noreply.github.com>
2021-01-21 14:19:56 +0330

    Ramin/Added_CRASH_BOOM_STEP_pngs (#83)
    * Added some CRASH, BOOM, STEP, pngs
    
    * Update deprecated annot
    
    * Removed SVGs and SymbolSVGPicture
    
    * Update deriv_chart.dart

--- lib/deriv_chart.dart
+++ lib/deriv_chart.dart
@@ -30,5 +30,4 @@ export 'src/widgets/market_selector/market_selector.dart';
 export 'src/widgets/market_selector/market_selector_button.dart';
 export 'src/widgets/market_selector/models.dart';
 export 'src/widgets/market_selector/symbol_icon.dart';
-export 'src/widgets/market_selector/symbol_svg_picture.dart';
 export 'src/x_axis/min_candle_duration_for_data_fit.dart';
