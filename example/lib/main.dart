932bffea18c78b89221b081f27574df30bccb67b
Osama Ghazal <124760982+osama-deriv@users.noreply.github.com>
2024-03-21 11:18:32 +0800

    fix: fix the api endpoint (#300)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,8 +69,8 @@ class FullscreenChart extends StatefulWidget {
 }
 
 class _FullscreenChartState extends State<FullscreenChart> {
-  static const String defaultAppID = '1089';
-  static const String defaultEndpoint = 'blue.binaryws.com';
+  static const String defaultAppID = '36544';
+  static const String defaultEndpoint = 'blue.derivws.com';
 
   List<Tick> ticks = <Tick>[];
   ChartStyle style = ChartStyle.line;
