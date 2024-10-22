6b33e9e84803ee17cfe938508fddfaebb1a9c95d
Ali Akbar <116874053+aliakbar-deriv@users.noreply.github.com>
2024-10-22 18:36:17 +0400

    Moved the formatEpochToGMTDateTime to helper_functions.dart (#339)

--- lib/src/deriv_chart/chart/data_visualization/drawing_tools/drawing_tool_label_painter.dart
+++ lib/src/deriv_chart/chart/data_visualization/drawing_tools/drawing_tool_label_painter.dart
@@ -1,7 +1,6 @@
 import 'package:deriv_chart/deriv_chart.dart';
 import 'package:deriv_chart/src/models/chart_config.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 /// Base class for drawing tool label painter.
 abstract class DrawingToolLabelPainter {
@@ -34,15 +33,5 @@ abstract class DrawingToolLabelPainter {
       Canvas canvas, Rect labelArea, Paint paint, TextPainter painter) {
     drawLabelBackground(canvas, labelArea, paint);
     paintWithTextPainter(canvas, painter: painter, anchor: labelArea.center);
-  }
-
-  /// Formats epoch to date time string.
-  String formatEpochToDateTime(int epochMillis) {
-    final DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(epochMillis);
-    final String formattedTime =
-        DateFormat('yy-MM-dd HH:mm:ss').format(dateTime);
-    final String timezone = dateTime.timeZoneName;
-
-    return '$formattedTime $timezone';
   }
 }
