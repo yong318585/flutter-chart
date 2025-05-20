b975718db0426e0edacc4ce85622d31979e06034
behnam-deriv <133759298+behnam-deriv@users.noreply.github.com>
2025-05-20 18:09:35 +0800

    fix: [GRWT-5941] Override shouldRepaint in vertical barrier (#384)
    * fix: override shouldRepaint in vertical barrier

--- lib/src/deriv_chart/chart/data_visualization/annotations/barriers/vertical_barrier/vertical_barrier.dart
+++ lib/src/deriv_chart/chart/data_visualization/annotations/barriers/vertical_barrier/vertical_barrier.dart
@@ -49,6 +49,14 @@ class VerticalBarrier extends Barrier {
   @override
   BarrierObject createObject() => VerticalBarrierObject(epoch!, quote: quote);
 
+  // Force repaint to ensure vertical barrier gets cleared when out of range.
+  // Safe since onPaint() in [VerticalBarrierPainter] checks
+  // series.isOnRange before drawing.
+  @override
+  bool shouldRepaint(covariant Barrier previous) {
+    return true;
+  }
+
   @override
   List<double> recalculateMinMax() =>
       isOnRange ? super.recalculateMinMax() : <double>[double.nan, double.nan];
